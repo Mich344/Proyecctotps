6f80ecd589460d9ff12e760cd486a9f95ba89187
Mich344 <104693039+Mich344@users.noreply.github.com>
2022-07-24 20:04:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 
 <!DOCTYPE html>
+
+<?php
+require 'config/database.php';
+$db = new databas();
+$con = db->conectar();
+$sql = $con->prepare("SELECT id, nombre, precio FROM productos WHERE activo=1");
+$sql ->execute();
+$resultado = $sql->fetchALL(PDO::FETCH_ASSOC);
+
+
+
+
+?>
+
+
+
 <html lang = "es">
 <head>
 <title>SportsWearLine</title>
