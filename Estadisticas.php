a8f6ebb34a8720c841c337cdfc83e03fd35d8561
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-08-29 14:22:11 -0500

    Update Estadisticas.php

--- Estadisticas.php
+++ Estadisticas.php
@@ -74,12 +74,12 @@
                 <div class="inner">
                   <h3>65</h3>
 
-                  <p>Unique Visitors</p>
+                  <p>Visitantes</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-pie-graph"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
             <!-- ./col -->
