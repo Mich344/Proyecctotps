1a1f779aece34c7d325a4151af969db392ccf736
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-08-29 14:22:39 -0500

    Update Estadisticas.php

--- Estadisticas.php
+++ Estadisticas.php
@@ -124,7 +124,7 @@
               <!-- DIRECT CHAT -->
               <div class="card direct-chat direct-chat-primary">
                 <div class="card-header">
-                  <h3 class="card-title">Direct Chat</h3>
+                  <h3 class="card-title"> Conversación Directa</h3>
 
                   <div class="card-tools">
                     <span title="3 New Messages" class="badge badge-primary">3</span>
